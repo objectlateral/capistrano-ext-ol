2aaf4edcd05f8cb88d946c972cea48fe15b48598
Jerod Santo <jerod.santo@gmail.com>
2014-02-13 11:45:44 -0600

    kill mulitple resque processes

--- lib/capistrano/ext/ol/resque.rb
+++ lib/capistrano/ext/ol/resque.rb
@@ -1,9 +1,15 @@
 Capistrano::Configuration.instance.load do
   namespace :resque do
-    desc "Kills resque workers so that monit will restart them"
+    desc "Kills all resque workers so that monit will restart them"
     task :restart do
-      pid = "#{shared_path}/pids/resque.pid"
-      run "if [ -f #{pid} ]; then kill -s QUIT `cat #{pid}` && rm -f #{pid}; fi"
+      run [
+        "for pid in #{shared_path}/pids/resque*.pid; do",
+        "if [ -f $pid ];",
+        "then kill -s QUIT `cat $pid`",
+        "&& rm -f $pid;",
+        "fi;",
+        "done"
+      ].join " "
     end
   end
 
